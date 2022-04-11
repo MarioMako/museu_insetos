f22b847256ca7b2f09aae67060ed5549a8b757de
MarioMako <marioeug.bastos@gmail.com>
2022-04-11 20:52:04 -0300

    primeiro commit

---
+++ src/main/java/com/ufra/museu_insetos/MuseuInsetosApplication.java
@@ -0,0 +1,13 @@
+package com.ufra.museu_insetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuseuInsetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuseuInsetosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ufra/museu_insetos/MuseuInsetosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufra.museu_insetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuseuInsetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
