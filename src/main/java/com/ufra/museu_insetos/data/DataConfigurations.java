1a76d86327ed9f24545b1b878c182ecd4afabcc1
= <=>
2022-04-18 21:25:15 -0300

    Configuração do banco de dados e alterações no pom

---
+++ src/main/java/com/ufra/museu_insetos/data/DataConfigurations.java
@@ -0,0 +1,43 @@
+package com.ufra.museu_insetos.data;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+
+public class DataConfigurations {
+
+    @Configuration
+    @Profile("dev")
+    public class DataConfiguration {
+        @Bean
+        public DataSource dataSource(){
+            DriverManagerDataSource dataSource = new DriverManagerDataSource();
+            dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+
+            dataSource.setUrl("jdbc:mysql://localhost:3306/tabela_insetos");
+            dataSource.setUsername("lnovik");
+            dataSource.setPassword("luannovik");
+            return dataSource;
+        }
+        @Bean
+        public JpaVendorAdapter jpaVendorAdapter(){
+            HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+            adapter.setDatabase(Database.MYSQL);
+            adapter.setShowSql(true);
+            adapter.setGenerateDdl(true);
+            adapter.setDatabasePlatform("org.hibernate.dialect.MySQL5Dialect");
+            adapter.setPrepareConnection(true);
+            return adapter;
+        }
+    }
+
+
+
+}
